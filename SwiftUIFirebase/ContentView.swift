86166f8116a0d5dfc85da476b08ea921b31b86ea
Nauxie <abhinav.tumu1@gmail.com>
2020-07-22 14:12:13 -0700

    Initial Commit

---
+++ SwiftUIFirebase/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebase
+//
+//  Created by Abhinav  Tumu on 7/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
